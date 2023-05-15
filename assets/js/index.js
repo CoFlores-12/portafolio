eadc24c18de53e45349684dcbd312228cb0ac757
CoFlores-12 <obethflores2014@gmail.com>
2023-05-15 17:37:16 -0400

    1.3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,96 @@
-const elements = document.getElementsByClassName('ani')
+const elements = document.getElementsByTagName('section')
 
 document.addEventListener('scroll', function (event) {
     checkElements();
-}, true /*Capture event*/);
+}, true);
 
 function checkElements() {
-    const trigger = window.innerHeight;
     for (let i = 0; i < elements.length; i++) {
         const elem = elements.item(i);
-        if (elem.getBoundingClientRect().top < trigger) {
-            elem.classList.remove('hid')
-            elem.classList.add(elem.getAttribute('ani'))
+        if (elem.getBoundingClientRect().top >= -200 && elem.getBoundingClientRect().top < 200) {
+            viewScene(elem)
         }else{
-            elem.classList.add('hid')
-            elem.classList.remove(elem.getAttribute('ani'))
+            disableScene(elem)
         }
     }
 }
 
-$(document).on("scroll", checkElements());
+checkElements();
+
+function viewScene(section) {
+    let els = $(section).find('.ani')
+    for (let i = 0; i < els.length; i++) {
+        els[i].classList.remove('hid')
+        els[i].classList.add(els[i].getAttribute('ani'))
+    }
+}
+
+function disableScene(section) {
+    let els = $(section).find('.ani')
+    for (let i = 0; i < els.length; i++) {
+        els[i].classList.add('hid')
+        els[i].classList.remove(els[i].getAttribute('ani'))
+    }
+}
+
+
+let currentSlide = 0;
+let imageCount = document.querySelectorAll(".imgsli");
+
+function startSlider() {
+    
+    if (imageCount.length === 0) {
+        imageCount = document.querySelectorAll(".imgsli");
+        images.style.transform = `translateX(0px)`;
+        return;
+    }
+    if (currentSlide === imageCount.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    
+    let images = document.querySelector(".ul2");
+    let images2 = document.querySelector(".ul1");
+    let images3 = document.querySelector(".ul3");
+    const vh = (document.documentElement.clientHeight*0.4)/(20/9);
+  
+  images.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+  images2.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+  images3.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+
+}
+
+function prevSlide() {
+    if (currentSlide ===  0) {
+        currentSlide = imageCount.length-1;
+    } else {
+          currentSlide--;
+      }
+      let images = document.querySelector(".ul2");
+      let images2 = document.querySelector(".ul1");
+      let images3 = document.querySelector(".ul3");
+      const vh = (document.documentElement.clientHeight*0.4)/(20/9);
+      images.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+    images2.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+    images3.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+}
+
+function nextSlide() {
+    if (currentSlide === imageCount.length - 1) {
+        currentSlide = 0;
+      } else {
+        currentSlide++;
+      }
+      let images = document.querySelector(".ul2");
+      let images2 = document.querySelector(".ul1");
+      let images3 = document.querySelector(".ul3");
+      const vh = (document.documentElement.clientHeight*0.4)/(20/9);
+      images.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+      images2.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+      images3.style.transform = `translateX(-${currentSlide * (vh)}px)`;
+}
+
+setInterval(() => {
+  startSlider();
+}, 5000);
