1dbae2015fdbf110ef8f53757bb422765a999941
CoFlores-12 <obethflores2014@gmail.com>
2023-09-11 15:56:20 -0700

    info

---
+++ assets/js/data.js
@@ -0,0 +1,43 @@
+const data = {
+    "adsbq11":{
+        name: "Deliverit",
+        sections: [
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/deli01.jpeg",
+                txt: "Splash Screen with elements animated"
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "./assets/images/deli02.jpeg",
+                txt: "Login with soft style"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/deliverit01.png",
+                txt: "multi categories"
+            },
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/deli03.jpeg",
+                txt: "map for delivery"
+            },
+            {
+                id: "del01",
+                style: 4,
+                img: "./assets/images/deli04.png",
+                txt: "payment with credit card"
+            }
+        ]
+    },
+    "adsbq12":{
+        name: "IS-Manager"
+    },
+    "adsbq21":{
+        name: "Makeups"
+    },
+}
