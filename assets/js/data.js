3af9e109af5347febba132f005aa605ea2793909
CoFlores-12 <obethflores2014@gmail.com>
2023-09-17 10:17:38 -0600

    images and animations

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,6 +1,10 @@
 const data = {
     "adsbq11":{
         name: "Deliverit",
+        desc:"<a href='https://deliverit-client.netlify.app/'>view in live</a>",
+        tech: [
+            "NodeJS","HTML5","CSS","JavaScript","MongDB","PWA"
+        ],
         sections: [
             {
                 id: "del01",
@@ -31,13 +35,159 @@ const data = {
                 style: 4,
                 img: "./assets/images/deli04.png",
                 txt: "payment with credit card"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/deli (1).jpeg",
+                txt: "admin system"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/deli (8).jpeg",
+                txt: "resume of orders"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/deli (7).jpeg",
+                txt: "users manager"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/deli (6).jpeg",
+                txt: "stores manager"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/deli (5).jpeg",
+                txt: ""
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "./assets/images/deli (4).jpeg",
+                txt: "roundsman site"
+            },
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/deli (3).jpeg",
+                txt: "allow take orders"
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "./assets/images/deli (2).jpeg",
+                txt: "resume order before take"
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "",
+                txt: "<a href='https://deliverit-client.netlify.app/'>view in live</a>"
             }
+            
+
         ]
     },
     "adsbq12":{
-        name: "IS-Manager"
+        name: "IS-Manager",
+        desc:"<a href='https://is-manager.vercel.app/'>view in live</a>",
+        tech: [
+            "NodeJS","HTML5","CSS","JavaScript","Puppeteer","ChartJS","Serverless","Chrome-AWS"
+        ],
+        sections: [
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/IS01.png",
+                txt: "Login with UNAH system"
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "./assets/images/IS01.jpeg",
+                txt: "dashboard individual"
+            },
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/IS02.jpeg",
+                txt: "monitor available classes"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/IS03.png",
+                txt: "study plan"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/IS03.1.jpeg",
+                txt: ""
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/IS03.jpeg",
+                txt: "individual progress map"
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "./assets/images/IS06.jpeg",
+                txt: "remaining classes monitor"
+            }
+        ]
     },
     "adsbq21":{
-        name: "Makeups"
+        name: "Makeups",
+        desc:"",
+        tech: [
+            "Python","HTML5","CSS","JavaScript","Flask"
+        ],
+        sections: [
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/m (2).jpeg",
+                txt: "Admin system"
+            },
+            {
+                id: "del01",
+                style: 2,
+                img: "./assets/images/m (1).jpeg",
+                txt: "dashboard"
+            },
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/m (6).jpeg",
+                txt: "form to add new post"
+            },
+            {
+                id: "del01",
+                style: 1,
+                img: "./assets/images/m (5).jpeg",
+                txt: "manager posts"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/m (4).jpeg",
+                txt: "clients view"
+            },
+            {
+                id: "del01",
+                style: 3,
+                img: "./assets/images/m (3).jpeg",
+                txt: ""
+            }
+        ]
     },
 }
